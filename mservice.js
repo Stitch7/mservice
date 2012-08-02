02435f1f4a4a329de8e8579c9bc117c604b4760c
Christopher Reitz <cre@mclient.localdomain>
2012-08-03 00:29:02 +0200

    [change] turned things around

--- mservice.js
+++ mservice.js
@@ -24,14 +24,15 @@
  */
 
 // libraries
-var $ = require('cheerio');
 var sys = require('sys');
 var http = require('http');
+
+var $ = require('cheerio');
 
 /**
  * gets man!ac ressorce with given action
  */
-var get = function(action, url) {
+var fetchManiacHtml = function(url, fn) {
 	http.get(url, function(response) {
 		//console.log(response);
 		var html = [];
@@ -43,7 +44,7 @@ var get = function(action, url) {
 
 		// when respond ends execute action 
 		response.on('end', function () {
-			action(html.join());
+			fn(html.join());
 		});
 	}).on('error', function(e) {
 		console.log("Got error: " + e.message);
@@ -59,88 +60,93 @@ var cleanManiacStuff = function(text) {
 /**
  * boards action, fetches man!ac board list
  */
-var boards = function (html) {
-	var boardList = [];
+var boards = function () {
+	var url = 'http://maniac-forum.de/forum/pxmboard.php';
+	
+	fetchManiacHtml(url, function (html) {
+		var boardList = [];
 
-	$(html).find('div table tr.bg2 td#norm:nth-child(2) a').each(function () {
-		var $a = $(this);
-		var href = $a.attr('href');
-		var hrefSearch = '?mode=board&brdid=';
-		if (href.indexOf(hrefSearch) != -1) {
-			var board = {};			
-			board.id = href.substring(href.indexOf(hrefSearch) + hrefSearch.length);
-			board.text = $a.html();
+		$(html).find('div table tr.bg2 td#norm:nth-child(2) a').each(function () {
+			var $a = $(this);
+			var href = $a.attr('href');
+			var hrefSearch = '?mode=board&brdid=';
+			if (href.indexOf(hrefSearch) != -1) {
+				var board = {};			
+				board.id = href.substring(href.indexOf(hrefSearch) + hrefSearch.length);
+				board.text = $a.html();
 
-			boardList.push(board);			
-		}
+				boardList.push(board);			
+			}
+		});
+
+		console.log(boardList);
 	});
-
-	console.log(boardList);
 };
 
 /**
  * threads action, fetches a man!ac thread list
  */
-var threads = function (html) {
-	var threadList = [];
-	var threads = $(html).find('body p').html().split('<br/>');
+var threads = function () {
+	var url = 'http://maniac-forum.de/forum/pxmboard.php?mode=threadlist&brdid=6'; // &sort=last&x=13&y=3
+	
+	fetchManiacHtml(url, function (html) {
+		var threadList = [];
+		var threads = $(html).find('body p').html().split('<br/>');
 
-	for(var i in threads) {
-		// Ignore empty lines TODO
-		if (threads[i].length < 500)  {
-			continue;
+		for(var i in threads) {
+			// Ignore empty lines TODO
+			if (threads[i].length < 500)  {
+				continue;
+			}
+
+			var $thread = $(threads[i]);
+			var $threadData = $thread.text().split("\n\r")
+
+			// threads id
+			var id = '';
+			var url = $($thread.find('a').get(1)).attr('href');
+			if (url !== undefined) {
+			  	id = url.substring(41)
+			}
+			
+			// threads subject
+			var subject = cleanManiacStuff($threadData[0]);
+			subject = subject.substring(0, subject.length - 2);
+			
+			// threads subject
+			var author = '';
+			if ($threadData[1] !== undefined) {
+				author = cleanManiacStuff($threadData[1]);
+			}
+			
+			// threads date
+			var date = ''; 
+			
+			// threads answer count 
+			var answerCount = ''; 
+
+			// threads date of last answer
+			var answerDate  = '';
+			if ($threadData[2] !== undefined) {
+				date        = cleanManiacStuff($threadData[2]).substring(3, 17);
+				answerCount = cleanManiacStuff($threadData[2]).substring(30, 35).replace(/\D/g, '');
+				answerDate  = cleanManiacStuff($threadData[2]).substring(42, 60);
+			}
+
+			// adding thread to list
+			threadList.push({
+				id: id,
+				author: author,
+				subject: subject,
+				date: date,
+				answerCount: answerCount,
+				answerDate: answerDate
+			});		
 		}
 
-		var $thread = $(threads[i]);
-		var $threadData = $thread.text().split("\n\r")
-
-		// threads id
-		var id = '';
-		var url = $($thread.find('a').get(1)).attr('href');
-		if (url !== undefined) {
-		  	id = url.substring(41)
-		}
-		
-		// threads subject
-		var subject = cleanManiacStuff($threadData[0]);
-		subject = subject.substring(0, subject.length - 2);
-		
-		// threads subject
-		var author = '';
-		if ($threadData[1] !== undefined) {
-			author = cleanManiacStuff($threadData[1]);
-		}
-		
-		// threads date
-		var date = ''; 
-		
-		// threads answer count 
-		var answerCount = ''; 
-
-		// threads date of last answer
-		var answerDate  = '';
-		if ($threadData[2] !== undefined) {
-			date        = cleanManiacStuff($threadData[2]).substring(3, 17);
-			answerCount = cleanManiacStuff($threadData[2]).substring(30, 35).replace(/\D/g, '');
-			answerDate  = cleanManiacStuff($threadData[2]).substring(42, 60);
-		}
-
-		// adding thread to list
-		threadList.push({
-			id: id,
-			author: author,
-			subject: subject,
-			date: date,
-			answerCount: answerCount,
-			answerDate: answerDate
-		});
-	}
-
-	console.log(threadList);
+		console.log(threadList);
+	});
 };
 
-var boardsUrl  = 'http://maniac-forum.de/forum/pxmboard.php';
-var threadsUrl = 'http://maniac-forum.de/forum/pxmboard.php?mode=threadlist&brdid=6'; // &sort=last&x=13&y=3
-
-get(threads, threadsUrl);
-//get(boards, boardsUrl);
+threads();
+boards();
