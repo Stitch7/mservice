f7ab448375c99f5836d1f717216172ca83558cc3
Christopher Reitz <christopher@reitz.re>
2016-12-22 23:16:40 +0100

    Add threadListScraper Test

---
+++ test/unit/scrapers/threadListScraperTest.js
@@ -0,0 +1,30 @@
+/**
+ * M!service
+ * Copyright(c) 2014-2017 Christopher Reitz.
+ * MIT Licensed
+ */
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+var helpers = require('./../../helpers.js');
+var scrapers = helpers.scrapers;
+var loadHtmlRessource = helpers.loadHtmlRessource;
+var loadJsonRessource = helpers.loadJsonRessource;
+
+describe('thradListScraper', function() {
+    it('it should parse html and return an array of threads', function(done) {
+        loadHtmlRessource('threadList', function (errorHtml, html) {
+            loadJsonRessource('threadList', function (errorJson, json) {
+                expect(errorHtml).to.be.null;
+                expect(errorJson).to.be.null;
+
+                var threadList = scrapers.threadList(html);
+                expect(threadList).to.eql(JSON.parse(json));
+
+                done();
+            });
+        });
+    });
+});
