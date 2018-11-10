34c7c37666f4cbbff2f24e0c85a3fd27af2952b0
Christopher Reitz <christopher@reitz.re>
2018-11-10 17:21:23 +0100

    Fix backup script for newer bash versions

--- backup.sh
+++ backup.sh
@@ -12,11 +12,11 @@ cd $BACKUP_DIR
 # Create dump
 docker exec -it $CONTAINER_NAME mongodump --db mservice -o /data/backup
 # Create tarball
-tar -cvzf $(CONTAINER_NAME).tar.gz mservice/
+tar -cvzf ${CONTAINER_NAME}.tar.gz mservice/
 # Encrypt tarball
-cat $SECRET | gpg -c --passphrase-fd 0 $(CONTAINER_NAME).tar.gz
+cat $SECRET | gpg -c --passphrase-fd 0 ${CONTAINER_NAME}.tar.gz
 # Move to OneDrive
-mv $(CONTAINER_NAME).tar.gz.gpg $(SYNC_DIR)/mservice/$(CONTAINER_NAME)_-_$(date +%Y-%m-%d_%H-%M-%S).tar.gz.gpg
+mv ${CONTAINER_NAME}.tar.gz.gpg ${SYNC_DIR}/mservice/${CONTAINER_NAME}_-_$(date +%Y-%m-%d_%H-%M-%S).tar.gz.gpg
 
 ## TODO: Remove old files
 # https://stackoverflow.com/questions/25785/delete-all-but-the-most-recent-x-files-in-bash
